4d1ebcda89884e2e18a250658bbc2e00c0d1eb4e
Anthony Osigbe <thronesofglory@gmail.com>
2023-07-23 15:34:38 -0700

    bug fix

--- string_output.c
+++ string_output.c
@@ -16,14 +16,14 @@ int printf_string(va_list val)
 	if (s == NULL)
 	{
 		s = "(null)";
-		len = _strlen(s);
+		len = strlen(s);
 		for (i = 0; i < len; i++)
 			_putchar(s[i]);
 		return (len);
 	}
 	else
 	{
-		len = _strlen(s);
+		len = strlen(s);
 		for (i = 0; i < len; i++)
 			_putchar(s[i]);
 		return (len);

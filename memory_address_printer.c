3d7a265e0bfba3827679e5f347905f593d83468f
Anthony Osigbe <thronesofglory@gmail.com>
2023-07-23 14:55:12 -0700

    memory_address_printer.c bug fix

--- memory_address_printer.c
+++ memory_address_printer.c
@@ -7,26 +7,26 @@
  */
 int printPointer(va_list val)
 {
-void *p;
-char *s = "(nil)";
-long int a;
-int b;
-int i;
+	void *p;
+	char *s = "(nil)";
+	long int a;
+	int b;
+	int i;
 
-p = va_arg(val, void*);
-if (p == NULL)
-{
-for (i = 0; s[i] != '\0'; i++)
-{
-_putchar(s[i]);
-}
-return (i);
-}
+	p = va_arg(val, void*);
+	if (p == NULL)
+	{
+		for (i = 0; s[i] != '\0'; i++)
+		{
+			_putchar(s[i]);
+		}
+		return (i);
+	}
 
-a = (unsigned long int)p;
-_putchar('0');
-_putchar('x');
-b = printf_hex_aux(a);
-return (b + 2);
+	a = (unsigned long int)p;
+	_putchar('0');
+	_putchar('x');
+	b = printHex(a);
+	return (b + 2);
 }
 

6e28a50b902a44f5e63e9c393111df2834a58415
Anthony Osigbe <thronesofglory@gmail.com>
2023-07-22 22:56:56 -0700

    hexadecimal_helpers.c

---
+++ hexadecimal_helpers.c
@@ -0,0 +1,38 @@
+#include "main.h"
+
+/**
+ * printHex - Prints an hexadecimal number.
+ * @num: The unsigned long integer to be printed as hexadecimal.
+ * Return: The number of characters printed.
+ */
+int printHex(unsigned long int num)
+{
+	long int i;
+	long int *array;
+	long int counter = 0;
+	unsigned long int temp = num;
+
+	while (num / 16 != 0)
+	{
+		num /= 16;
+		counter++;
+	}
+	counter++;
+	array = malloc(counter * sizeof(long int));
+
+	for (i = 0; i < counter; i++)
+	{
+		array[i] = temp % 16;
+		temp /= 16;
+	}
+	for (i = counter - 1; i >= 0; i--)
+	{
+		if (array[i] > 9)
+			array[i] = array[i] + 39;
+		_putchar(array[i] + '0');
+	}
+
+	free(array);
+
+	return (counter);
+}

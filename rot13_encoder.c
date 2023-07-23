95186d10817adead0f384e89be1e05e33f68cff2
Anthony Osigbe <thronesofglory@gmail.com>
2023-07-23 16:50:07 -0700

    bug fix

--- rot13_encoder.c
+++ rot13_encoder.c
@@ -1,11 +1,11 @@
 #include "main.h"
 
 /**
- * printRot13 - Prints a string in ROT13 cipher.
+ * printf_rot13 - Prints a string in ROT13 cipher.
  * @args: Type struct va_arg where is allocated printf arguments.
  * Return: The number of characters printed.
  */
-int printRot13(va_list args)
+int printf_rot13(va_list args)
 {
 	int i, j, counter = 0;
 	int k = 0;

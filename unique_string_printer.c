c8a7f005cee31df27be85e7b52da3f527b993f60
Anthony Osigbe <thronesofglory@gmail.com>
2023-07-22 22:55:36 -0700

    unique_string_printer.c

---
+++ unique_string_printer.c
@@ -0,0 +1,42 @@
+#include "main.h"
+
+/**
+ * printExclusiveString - Prints a string with non-printable characters,
+ * represented as hexadecimal escape sequences.
+ * @val: Variable argument list containing the arguments.
+ * Return: The length of the printed string.
+ */
+int printExclusiveString(va_list val)
+{
+	char *s;
+	int i, len = 0;
+	int cast;
+
+	s = va_arg(val, char *);
+	if (s == NULL)
+		s = "(null)";
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (s[i] < 32 || s[i] >= 127)
+		{
+			_putchar('\\');
+			_putchar('x');
+			len += 2;
+			cast = (int)s[i];
+
+			if (cast < 16)
+			{
+				_putchar('0');
+				len++;
+			}
+			len += printf_HEX_aux(cast);
+		}
+		else
+		{
+			_putchar(s[i]);
+			len++;
+		}
+	}
+
+	return (len);
+}

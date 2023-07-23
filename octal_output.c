13658063f2c97734bbdd2430dc11112b9d72024a
Anthony Osigbe <thronesofglory@gmail.com>
2023-07-22 22:57:56 -0700

    octal_output.c

---
+++ octal_output.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+ * printOct - Prints an octal number.
+ * @val: Variable argument list containing the arguments.
+ * Return: The number of characters printed.
+ */
+int printOct(va_list val)
+{
+	int i;
+	int *array;
+	int counter = 0;
+	unsigned int num = va_arg(val, unsigned int);
+	unsigned int temp = num;
+
+	while (num / 8 != 0)
+	{
+		num /= 8;
+		counter++;
+	}
+	counter++;
+	array = malloc(counter * sizeof(int));
+
+	for (i = 0; i < counter; i++)
+	{
+		array[i] = temp % 8;
+		temp /= 8;
+	}
+	for (i = counter - 1; i >= 0; i--)
+	{
+		_putchar(array[i] + '0');
+	}
+	free(array);
+
+	return (counter);
+}

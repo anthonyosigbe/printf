a9f5fd63aa830db0f82c200a91cd5d79056941b8
Anthony Osigbe <thronesofglory@gmail.com>
2023-07-23 17:31:54 -0700

    bug fix

--- _putchar.c
+++ _putchar.c
@@ -1,8 +1,11 @@
-#include <unistd.h>
+#include "main.h"
+
 /**
- * _putchar - write character c to the stdout
- * @c: the character
- * Return: 1 on success and -1 otherwise
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
  */
 int _putchar(char c)
 {
